633fa55423a702740c63b3b24a055807fb0f8384
haseeb0807 <abdulhaseeb.chouhan@zemosolabs.com>
2022-12-01 14:09:20 +0530

    implement singleton design pattern

---
+++ creational/singleton/src/SingleObject.java
@@ -0,0 +1,14 @@
+public class SingleObject {
+    private static SingleObject instance = new SingleObject();
+
+    //make the constructor private so that this class cannot be instantiated
+    private SingleObject(){}
+
+    public static SingleObject getInstance(){
+        return instance;
+    }
+
+    public void showMessage(){
+        System.out.println("Hello World!!!");
+    }
+}
