2647a9c90b634b2a2c8ae96a85c78d79c877631f
haseeb0807 <abdulhaseeb.chouhan@zemosolabs.com>
2022-12-06 23:13:08 +0530

    Adaptor design pattern

---
+++ structural/adaptor/src/SocketAdaptor.java
@@ -0,0 +1,5 @@
+public interface SocketAdaptor {
+    public Voltage getVoltageOf240V();
+    public Voltage getVoltageOf12V();
+    public Voltage getVoltageOf3V();
+}
---
+++ structural/adaptor/src/Voltage.java
@@ -0,0 +1,18 @@
+public class Voltage {
+    private int voltage;
+
+    public Voltage(int v)
+    {
+        this.voltage = v;
+    }
+
+    public int getVolts()
+    {
+        return voltage;
+    }
+
+    public void setVolts(int voltage)
+    {
+        this.voltage = voltage;
+    }
+}
